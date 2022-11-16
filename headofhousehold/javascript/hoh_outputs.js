25a191cf903b8dc9b5eba74e5d2b9019829c3f85
Jay Martin <jaydavidmartin@gmail.com>
2022-11-16 02:39:29 -0600

    Adjusted HOH Page Style

--- headofhousehold/javascript/hoh_outputs.js
+++ headofhousehold/javascript/hoh_outputs.js
@@ -1,11 +1,11 @@
 /*Default values */
-document.getElementById('HOH_savings').innerHTML = 'The head of household filing status saves you <b>$' + 862 + '</b>.';
-document.getElementById('item_or_stand').innerHTML = 'You would use the <em>standard deduction</em> as both a single filer and head of household.';
+document.getElementById('HOH_savings').innerHTML = 'The head of household filing status saves you <b>$' + 862 + '</b> compared to filing as single.';
+document.getElementById('item_or_stand').innerHTML = 'You would use the <em>standard deduction</em> as both a single filer and a head of household.';
 
 /* Calculates tax savings from the head of household status*/
 function difference_hoh(){
     user_dif = taxDifferenceatIncomeValue(myRange_HOH.value, myRange_ID.value);
-    document.getElementById('HOH_savings').innerHTML = 'The head of household filing status saves you <b>$' + user_dif.toFixed().toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + '</b>.';
+    document.getElementById('HOH_savings').innerHTML = 'The head of household filing status saves you <b>$' + user_dif.toFixed().toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + '</b> compared to filing as single.';
 }
 
 /* Writes to page whether user would itemize or use the standard deduction */
@@ -14,7 +14,7 @@ function deductType(){
     hoh_deduct = hoh_deduction(myRange_ID.value);
 
     if(single_deduct===12950 && hoh_deduct===19400){
-        document.getElementById('item_or_stand').innerHTML = 'You would use the <em>standard deduction</em> as both a single filer and head of household.';
+        document.getElementById('item_or_stand').innerHTML = 'You would use the <em>standard deduction</em> as both a single filer and a head of household.';
     }
     else if(single_deduct>12950 && hoh_deduct===19400){
         document.getElementById('item_or_stand').innerHTML = 'You would use the <em>standard deduction</em> as a head of household but would <em>itemize</em> as a single filer.';
