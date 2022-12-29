5ecd4e978510b895727acf29533c816d0085c5d4
Jay Martin <jaydavidmartin@gmail.com>
2022-12-28 22:08:19 -0600

    Handle window resize events
    Highlights are now resized when the window is resized.

--- reference_files/javascript/on_startup.js
+++ reference_files/javascript/on_startup.js
@@ -22,33 +22,70 @@ for (var i = 0; i < highlights.length; i++) {
 }
 
 //Determine whether to initialize desktop or mobile page
+//Wait until page is fully loaded
 var displayWidth = window.innerWidth;
-if(displayWidth < 900){
-	initialize_mobile_page();
-	isMobile = true; //this is used by the functions that open the highlights when clicked
+$(document).ready(function(){
+	desktop_or_mobile_intialize();
+});
+
+//Handle screen resize events
+var windowWidth = $(window).width();
+$(window).on('resize', function() {
+	if ($(this).width() !== windowWidth) {
+    	windowWidth = $(this).width();
+    	prepare_resize();
+  	}
+});
+
+function prepare_resize(){
+	// Hide highlights, show loader
+	$('#loader_container').css('display', 'block');
+	$('#highlights_content').css('visibility', 'hidden');
+	$('#programs').css('visibility', 'hidden');
+
+	// Adjust borders
+	$('#highlights').css('border', 'none');
+	$('#highlights_title').css({
+		'border'        : 'solid',
+		'border-color'  : '#adadad',
+		'border-width'  : '1.5px'
+	});
+
+	//Expand highlights & programs
+	$('.explanation_and_animation_button_container').css('height', '100%');
+	$('#programs').css('height', '100%');
+
+	//run the inititalization function
+	desktop_or_mobile_intialize();
 }
-else{
-	initialize_page();
+
+function desktop_or_mobile_intialize(){
+	if(displayWidth < 900){
+		initialize_mobile_page();
+		isMobile = true; //this is used by the functions that open the highlights when clicked
+	}
+	else{
+		initialize_page();
+		isMobile = false;
+	}
 }
 
 function initialize_page(){
-	timer = 1000;
+	//calculate programs height and set to zero
+	programsHeight = window.getComputedStyle(document.getElementById('programs')).height;
+	document.getElementById("programs").style.height = "0px";
+
+	//calculate the heights of each of the highlights and then set each to zero
+	for (var i = 0; i < highlights_heights.length; i++) {
+		id = 'highlights_container' + (i+1).toString();
+		highlights_heights[i] = window.getComputedStyle(document.getElementById(id)).height;
+		document.getElementById(id).style.height = "0px";
+	}
+
+	//make highlights and programs visible, set highlights & programs transition time to .5s ease
 	setTimeout(function () {
-		//calculate highlights heights, as well as list of programs height, and then set to zero
-		programsHeight = window.getComputedStyle(document.getElementById('programs')).height;
-		document.getElementById("programs").style.height = "0px";
-
-		for (var i = 0; i < highlights_heights.length; i++) {
-			id = 'highlights_container' + (i+1).toString();
-			highlights_heights[i] = window.getComputedStyle(document.getElementById(id)).height;
-			document.getElementById(id).style.height = "0px";
-		}
-
-	}, timer);
-
-	//make highlights and programs visible
-	timer += 1500;
-	setTimeout(function () {
+		$('.explanation_and_animation_button_container').css('transition', 'height .5s ease');
+		$('.programs').css('transition', 'height .5s ease');
 		$('#highlights').css({
 			'border'       : 'solid',
 			'border-color' : '#adadad',
@@ -63,24 +100,20 @@ function initialize_page(){
 		document.getElementById("loader_container").style.display = 'none';
 		document.getElementById("highlights_content").style.visibility = 'visible';
 		document.getElementById("programs").style.visibility = 'visible';
-	}, timer);
+	}, 0);
 }
 
 function initialize_mobile_page(){
-	timer = 1000;
-	setTimeout(function () {
-		/* calculate highlights heights, and then set to zero */
-		for (var i = 0; i < highlights_heights.length; i++) {
-			id = 'mobile_highlights_container' + (i+1).toString();
-			highlights_heights_mobile[i] = window.getComputedStyle(document.getElementById(id)).height;
-			document.getElementById(id).style.height = "0px";
-		}
+	// calculate highlights heights, and then set to zero
+	for (var i = 0; i < highlights_heights.length; i++) {
+		id = 'mobile_highlights_container' + (i+1).toString();
+		highlights_heights_mobile[i] = window.getComputedStyle(document.getElementById(id)).height;
+		document.getElementById(id).style.height = "0px";
+	}
 
-	}, timer);
-
-	/* make highlights visible */
-	timer += 1500;
+	// make highlights visible
 	setTimeout(function () {
+		$('.explanation_and_animation_button_container_mobile').css('transition', 'height .5s ease');
 		$('#highlights_mobile').css({
 			'border'       : 'solid',
 			'border-color' : '#adadad',
@@ -94,6 +127,6 @@ function initialize_mobile_page(){
 		});
 		document.getElementById("loader_container_mobile").style.display = 'none';
 		document.getElementById("highlights_content_mobile").style.visibility = 'visible';
-	}, timer);
+	}, 100);
 }
 
