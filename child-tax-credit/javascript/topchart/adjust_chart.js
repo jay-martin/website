3e5b8aa8dd8ff48e614f8118e533bf96a9e1c08e
Jay Martin <jaydavidmartin@gmail.com>
2023-01-16 22:19:04 -0600

    Chart functionality

--- child-tax-credit/javascript/topchart/adjust_chart.js
+++ child-tax-credit/javascript/topchart/adjust_chart.js
@@ -31,4 +31,49 @@ function adjust_curve(){
     else if(filingStatus === 'married'){
         married_ctc_builder_2023(topChart, 'x', 'ctc', numChildren);
     }
+
+    // Adjust x-axis tick values
+    x_axis_ticks();
 }
+
+/* Zooms on Lower Incomes */
+function x_axis_ticks(){
+    if(filing_status.value === 'married'){
+        topChart.internal.config.axis_x_tick_values = [0, 50000, 100000, 150000, 200000, 250000, 300000, 350000, 400000, 450000, 500000];
+    }
+    else{
+        topChart.internal.config.axis_x_tick_values = [0, 25000, 50000, 75000, 100000, 125000, 150000, 175000, 200000, 225000, 250000, 275000, 300000, 325000];
+    }
+}
+
+/* Zooms on Lower Incomes */
+function zoom_top_chart(){
+    if(top_chart_zoom.checked){
+        user_income.step = 100;
+        if( filing_status.value === 'single' && (num_children.value === 'one' || num_children.value === 'two') ) {
+            topChart.internal.config.axis_x_tick_values = [0, 5000, 10000, 15000, 20000, 25000];
+            topChart.axis.max({x: 25000});
+            user_income.max = 25000;
+        }
+        else if( filing_status.value === 'hoh' && (num_children.value === 'one')){
+            topChart.internal.config.axis_x_tick_values = [0, 5000, 10000, 15000, 20000, 25000, 30000, 35000];
+            topChart.axis.max({x: 35000});
+            user_income.max = 35000;
+        }
+        else{
+            topChart.internal.config.axis_x_tick_values = [0, 10000, 20000, 30000, 40000, 50000];
+            topChart.axis.max({x: 50000});
+            user_income.max = 50000;
+        }
+    }
+    else {
+        x_axis_ticks();
+        topChart.internal.config.axis_x_max = undefined;
+        modify_income(); //needed to force axis re-adjust
+        user_income.step = 1000;
+        user_income.max = 240000;
+    }
+}
+
+
+
