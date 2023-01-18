e6e2a409196e1bf28ec7aea13a7ae02b65c3424f
Jay Martin <jaydavidmartin@gmail.com>
2023-01-18 02:42:51 -0600

    CDCC Chart; Adjusted Article Order
    Added the CDCC chart & animations to homepage; moved the "Bills" section below the "Programs" section on the homepage

--- home/javascript/charts/programs/cdcc.js
+++ home/javascript/charts/programs/cdcc.js
@@ -9,36 +9,43 @@ var cdccChart = c3.generate({
     },
     data: {
         xs: {
-            'HOH_Savings' : 'x',
-            'person1' : 'x1',
-            'person2' : 'x2',
-            'after_ctc' : 'x3',
-
-            'point' : 'x_point',
+            'credit_rate'   : 'x_credit_rate',
+            'credit_amount' : 'x_credit_amount',
+            
+            'point_credit_rate' : 'x_point',
+            'point_credit_amount' : 'x_point',
         },
         columns: [
-            ['x',            0, 12950, 19400, 23225, 34050, 54725, 75300, 102025, 108450, 183000, 189450, 228900, 235350, 552850, 559300, 600000],
-            ['HOH_Savings',  0, 0,     645,   645,   861.5, 861.5, 2919,  2919,   3047.5, 3047.5, 3563.5, 3563.5, 3757,   3757,   3886,   3886],
+            ['x_credit_amount', 0, 13850, 23000, 23001, 25000, 25001, 27000, 27001, 29000, 29001, 31000, 31001, 33000, 33001, 35000, 35001, 37000, 37001, 39000, 39001, 41000, 41001, 43000, 43001, 60000],
+            ['credit_amount',   0, 0,     915,   900,   900,   870,   870,   840,   840,   810,   810,   780,   780,   750,   750,   720,   720,   690,   690,   660,   660,   630,   630,   600,   600],
 
-            ['x_point', 50000],
-            ['point', 862],
+            ['x_credit_rate', 0,   15000, 15001, 17000, 17001, 19000, 19001, 21000, 21001, 23000, 23001, 25000, 25001, 27000, 27001, 29000, 29001, 31000, 31001, 33000, 33001, 35000, 35001, 37000, 37001, 39000, 39001, 41000, 41001, 43000, 43001, 60000],
+            ['credit_rate',   .35, .35,   .34,   .34,   .33,   .33,   .32,   .32,   .31,   .31,   .30,   .30,   .29,   .29,   .28,   .28,   .27,   .27,   .26,   .26,   .25,   .25,   .24,   .24,   .23,   .23,   .22,   .22,   .21,   .21,   .20,   .20],
+
+            ['x_point',             20000],
+            ['point_credit_amount', 615],
+            ['point_credit_rate',   .32],
+            
         ],
-        types: {
-            HOH_Savings: 'area',
-            person1: 'area',
-            person2: 'area',
-            after_ctc: 'area',
+        axes: {
+            credit_rate         : 'y',
+            point_credit_rate   : 'y',
+            credit_amount       : 'y2',
+            point_credit_amount : 'y2',
         },
         names: {
-            HOH_Savings: 'HOH Tax Savings',
-            person1: 'Person 1',
-            person2: 'Person 2',
-            after_ctc: 'HOH Tax Savings after CTC'
+            credit_rate:   'Credit Rate',
+            credit_amount: 'Credit Amount',
+        },
+        types: {
+            'credit_amount' : 'area',
         },
         colors: {
-            HOH_Savings : '#f7c22f',
-            after_ctc   : '#6ab6fc',
-            point       :  white_or_black,
+            credit_rate       : '#6ab6fc',
+            point_credit_rate : '#6ab6fc',
+
+            credit_amount       : '#f7c22f',
+            point_credit_amount : '#f7c22f',
         },
     },
     transition: {
@@ -46,13 +53,13 @@ var cdccChart = c3.generate({
     },
     padding: {
         bottom: 0,
-        top: 10,
+        top: 0,
         left: 65,
-        right: 20,
+        right: 65,
     },
     legend: {
         position: 'bottom',
-        hide: ['point'],
+        hide: ['point_credit_rate', 'point_credit_amount'],
     },
     tooltip: {
         show: false
@@ -62,31 +69,41 @@ var cdccChart = c3.generate({
             label: {text: 'Employment Income', position: 'outer-center'},
             tick: {
                 format: d3.format('$,'),
-                values: [0, 100000, 200000, 300000, 400000, 500000, 600000]
+                values: [0, 15000, 30000, 45000, 60000]
             },
             padding: {left: 0, right: 0},
-            max: 600000,
+            max: 60000,
         },
         y: {
-            label: {text: 'Tax Savings', position: 'outer-middle'},
+            show: true,
+            label: {text: 'Credit Rate', position: 'outer-middle'},
+            tick: {
+                format: d3.format('.0%'),
+                values: [0, .05, .1, .15, .2, .25, .3, .35, .4]
+            },
+            max: .4,
+            min: 0,
+            padding: {top: 0, bottom: 0},
+        },
+        y2: {
+            show: true,
+            label: {text: 'Credit Amount', position: 'outer-middle'},
             tick: {
                 format: d3.format('$,'),
-                values: [0, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 5000, 6000]
+                values: [0, 250, 500, 750, 1000]
             },
-            max: 4000,
-            padding: {top: 0},
-        }
+            max: 1200,
+            padding: {top: 0, bottom: 0},
+        },
     },
     grid: {
         lines: {
           front: false
         },
         x: {
-            lines: [{value: 50000, text: 'Your income'}],
+            lines: [{value: 20000,}],
             min: 0,
         },
-        y: {
-        }
     }
 });
 
@@ -97,36 +114,43 @@ var tabletCdccChart = c3.generate({
     },
     data: {
         xs: {
-            'HOH_Savings' : 'x',
-            'person1' : 'x1',
-            'person2' : 'x2',
-            'after_ctc' : 'x3',
-
-            'point' : 'x_point',
+            'credit_rate'   : 'x_credit_rate',
+            'credit_amount' : 'x_credit_amount',
+            
+            'point_credit_rate' : 'x_point',
+            'point_credit_amount' : 'x_point',
         },
         columns: [
-            ['x',            0, 12950, 19400, 23225, 34050, 54725, 75300, 102025, 108450, 183000, 189450, 228900, 235350, 552850, 559300, 600000],
-            ['HOH_Savings',  0, 0,     645,   645,   861.5, 861.5, 2919,  2919,   3047.5, 3047.5, 3563.5, 3563.5, 3757,   3757,   3886,   3886],
+            ['x_credit_amount', 0, 13850, 23000, 23001, 25000, 25001, 27000, 27001, 29000, 29001, 31000, 31001, 33000, 33001, 35000, 35001, 37000, 37001, 39000, 39001, 41000, 41001, 43000, 43001, 60000],
+            ['credit_amount',   0, 0,     915,   900,   900,   870,   870,   840,   840,   810,   810,   780,   780,   750,   750,   720,   720,   690,   690,   660,   660,   630,   630,   600,   600],
 
-            ['x_point', 50000],
-            ['point', 862],
+            ['x_credit_rate', 0,   15000, 15001, 17000, 17001, 19000, 19001, 21000, 21001, 23000, 23001, 25000, 25001, 27000, 27001, 29000, 29001, 31000, 31001, 33000, 33001, 35000, 35001, 37000, 37001, 39000, 39001, 41000, 41001, 43000, 43001, 60000],
+            ['credit_rate',   .35, .35,   .34,   .34,   .33,   .33,   .32,   .32,   .31,   .31,   .30,   .30,   .29,   .29,   .28,   .28,   .27,   .27,   .26,   .26,   .25,   .25,   .24,   .24,   .23,   .23,   .22,   .22,   .21,   .21,   .20,   .20],
+
+            ['x_point',             20000],
+            ['point_credit_amount', 615],
+            ['point_credit_rate',   .32],
+            
         ],
-        types: {
-            HOH_Savings: 'area',
-            person1: 'area',
-            person2: 'area',
-            after_ctc: 'area',
+        axes: {
+            credit_rate         : 'y',
+            point_credit_rate   : 'y',
+            credit_amount       : 'y2',
+            point_credit_amount : 'y2',
         },
         names: {
-            HOH_Savings: 'HOH Tax Savings',
-            person1: 'Person 1',
-            person2: 'Person 2',
-            after_ctc: 'HOH Tax Savings after CTC'
+            credit_rate:   'Credit Rate',
+            credit_amount: 'Credit Amount',
+        },
+        types: {
+            'credit_amount' : 'area',
         },
         colors: {
-            HOH_Savings : '#f7c22f',
-            after_ctc   : '#6ab6fc',
-            point       :  white_or_black,
+            credit_rate       : '#6ab6fc',
+            point_credit_rate : '#6ab6fc',
+
+            credit_amount       : '#f7c22f',
+            point_credit_amount : '#f7c22f',
         },
     },
     transition: {
@@ -134,13 +158,13 @@ var tabletCdccChart = c3.generate({
     },
     padding: {
         bottom: 0,
-        top: 10,
+        top: 0,
         left: 65,
-        right: 20,
+        right: 65,
     },
     legend: {
         position: 'bottom',
-        hide: ['point'],
+        hide: ['point_credit_rate', 'point_credit_amount'],
     },
     tooltip: {
         show: false
@@ -150,30 +174,129 @@ var tabletCdccChart = c3.generate({
             label: {text: 'Employment Income', position: 'outer-center'},
             tick: {
                 format: d3.format('$,'),
-                values: [0, 100000, 200000, 300000, 400000, 500000, 600000]
+                values: [0, 15000, 30000, 45000, 60000],
             },
             padding: {left: 0, right: 0},
-            max: 600000,
+            max: 60000,
         },
         y: {
-            label: {text: 'Tax Savings', position: 'outer-middle'},
+            show: true,
+            label: {text: 'Credit Rate', position: 'outer-middle'},
+            tick: {
+                format: d3.format('.0%'),
+                values: [0, .05, .1, .15, .2, .25, .3, .35, .4]
+            },
+            max: .4,
+            min: 0,
+            padding: {top: 0, bottom: 0},
+        },
+        y2: {
+            show: true,
+            label: {text: 'Credit Amount', position: 'outer-middle'},
             tick: {
                 format: d3.format('$,'),
-                values: [0, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 5000, 6000]
+                values: [0, 250, 500, 750, 1000]
             },
-            max: 4000,
-            padding: {top: 0},
-        }
+            max: 1200,
+            padding: {top: 0, bottom: 0},
+        },
     },
     grid: {
         lines: {
           front: false
         },
         x: {
-            lines: [{value: 50000, text: 'Your income'}],
+            lines: [{value: 20000, text: 'Your income'}],
             min: 0,
         },
-        y: {
-        }
     }
 });
+
+/********************* Animation ******************************************************************************************************************************/
+// Initiate animation
+cdcc_stage = 0;
+var cdcc_timeout;
+function cdcc_animation() {
+    // Start the animation loop.
+    if(cdcc_stage == 0){
+        cdcc_timeout = setTimeout(cdcc_stage_1, 0);
+    }
+    else if(cdcc_stage == 1){
+        cdcc_timeout = setTimeout(cdcc_stage_2, 0);
+    }
+    else if(cdcc_stage == 2){
+        cdcc_timeout = setTimeout(cdcc_stage_3, 0);
+    }
+    else if(cdcc_stage == 3){
+        cdcc_timeout = setTimeout(cdcc_stage_4, 0);
+    }
+}
+
+// Cancel the pending setTimeout calls (triggered when user scrolls off of html element)
+function mouse_out_cdcc(event) {
+    clearTimeout(cdcc_timeout);
+}
+
+// Stage 1
+function cdcc_stage_1(){
+    cdccChart.xgrids([ {value: 36000}, ]);
+    cdccChart.load({
+        columns: [ 
+            ['x_point',             36000],
+            ['point_credit_amount', 720],
+            ['point_credit_rate',   .24],
+        ]
+    });
+
+    cdcc_stage++;
+    cdcc_timeout = setTimeout(cdcc_stage_2, 1000);
+}
+
+// Stage 2
+function cdcc_stage_2(){
+    cdccChart.load({
+        columns: [ 
+            ['x_credit_amount', 0, 27700, 35000, 35001, 37000, 37001, 39000, 39001, 41000, 41001, 43000, 43001, 60000],
+            ['credit_amount',   0, 0,     730,   720,   720,   690,   690,   660,   660,   630,   630,   600,   600],
+
+            ['x_point',             36000],
+            ['point_credit_amount', 720],
+            ['point_credit_rate',   .24],
+        ]
+    });
+
+    cdcc_stage++;
+    cdcc_timeout = setTimeout(cdcc_stage_3, 1000);
+}
+
+// Stage 3
+function cdcc_stage_3(){
+    cdccChart.xgrids([ {value: 20000}, ]);
+    cdccChart.load({
+        columns: [ 
+            ['x_point',             20000],
+            ['point_credit_amount', 0],
+            ['point_credit_rate',   .32],
+        ]
+    });
+
+    cdcc_stage++;
+    cdcc_timeout = setTimeout(cdcc_stage_4, 1000);
+}
+
+// Stage 4
+function cdcc_stage_4(){
+    cdccChart.load({
+        columns: [ 
+            ['x_credit_amount', 0, 13850, 23000, 23001, 25000, 25001, 27000, 27001, 29000, 29001, 31000, 31001, 33000, 33001, 35000, 35001, 37000, 37001, 39000, 39001, 41000, 41001, 43000, 43001, 60000],
+            ['credit_amount',   0, 0,     915,   900,   900,   870,   870,   840,   840,   810,   810,   780,   780,   750,   750,   720,   720,   690,   690,   660,   660,   630,   630,   600,   600],
+
+            ['x_point',             20000],
+            ['point_credit_amount', 615],
+            ['point_credit_rate',   .32],
+        ]
+    });
+
+    cdcc_stage = 0;
+    cdcc_timeout = setTimeout(cdcc_stage_1, 1000);
+}
