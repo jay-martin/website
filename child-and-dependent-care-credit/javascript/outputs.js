b544c7acaac6418f245bebc313c8a3b4a448c518
Jay Martin <jaydavidmartin@gmail.com>
2023-01-18 03:12:19 -0600

    Minor Wording Fixes

--- child-and-dependent-care-credit/javascript/outputs.js
+++ child-and-dependent-care-credit/javascript/outputs.js
@@ -5,7 +5,7 @@ function top_chart_outputs(){
     numChildren = num_children.value;
 
     credit_amount = cdcc_amount_2023(income, filingStatus, numChildren).toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-    document.getElementById('credit_amount').innerHTML = 'Your credit is worth up to: <b>$' + credit_amount + '</b>';
+    document.getElementById('credit_amount').innerHTML = 'You can subtract up to <b>$' + credit_amount + '</b> from your taxes';
 }
 
 /* Outputs CDCC Values to the complexity chart */
@@ -17,6 +17,6 @@ function complexity_chart_outputs(){
     credit_amount = cdcc_amount_2023(income, filingStatus, numChildren).toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
     credit_rate = (cdcc_rate(income) * 100).toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
     
-    document.getElementById('complexity_credit_amount').innerHTML = 'Your credit is worth up to: <b>$' + credit_amount + '</b>';
+    document.getElementById('complexity_credit_amount').innerHTML = 'You can subtract up to <b>$' + credit_amount + '</b> from your taxes';
     document.getElementById('complexity_credit_rate').innerHTML = 'The credit rate at your income is: <b>' + credit_rate + '%</b>';
 }
